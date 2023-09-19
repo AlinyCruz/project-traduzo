9e5c64bcaf9d73b8001657eb195f86b2bdf7924c
Aline Oliveira Cruz <aliny.cruz@yahoo.com.br>
2023-09-19 13:48:32 -0300

    Requisito 06

--- src/controllers/translate_controller.py
+++ src/controllers/translate_controller.py
@@ -49,4 +49,20 @@ def index():
 # Req. 6
 @translate_controller.route("/reverse", methods=["POST"])
 def reverse():
-    raise NotImplementedError
+    languages = LanguageModel.list_dicts()
+    text_to_translate = request.form.get("text-to-translate")
+    translate_from = request.form.get("translate-from")
+    translate_to = request.form.get("translate-to")
+    translator2 = GoogleTranslator(
+        source=translate_from, target=translate_to
+    )
+    translator3 = translator2.translate(text_to_translate)
+
+    return render_template(
+        "index.html",
+        languages=languages,
+        text_to_translate=text_to_translate,
+        translate_from=translate_to,
+        translate_to=translate_from,
+        translated=translator3,
+    )
