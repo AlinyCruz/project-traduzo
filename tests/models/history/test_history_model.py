60b575badeccb09a9fbc03829ac5b8c69cbbebf7
Aline Oliveira Cruz <aliny.cruz@yahoo.com.br>
2023-09-19 14:30:16 -0300

    Requisito 07

--- tests/models/history/test_history_model.py
+++ tests/models/history/test_history_model.py
@@ -1,7 +1,25 @@
-# import json
-# from src.models.history_model import HistoryModel
+import json
+from src.models.history_model import HistoryModel
 
 
 # Req. 7
 def test_request_history():
-    raise NotImplementedError
+    list = [
+        {
+            "text_to_translate": "Hello, I like videogame",
+            "translate_from": "en",
+            "translate_to": "pt",
+        },
+        {
+            "text_to_translate": "Do you love music?",
+            "translate_from": "en",
+            "translate_to": "pt",
+        },
+    ]
+    response = json.loads(HistoryModel.list_as_json())
+    expected = list
+
+    for object in response:
+        object.pop("_id")
+
+    assert response == expected
