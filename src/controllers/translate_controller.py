41ca73629e9714f0d262d26ec9f27bfc663f39f9
Aline Oliveira Cruz <aliny.cruz@yahoo.com.br>
2023-09-19 12:03:15 -0300

    Requisito 05

--- src/controllers/translate_controller.py
+++ src/controllers/translate_controller.py
@@ -1,7 +1,8 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request
 
-# from deep_translator import GoogleTranslator
+from deep_translator import GoogleTranslator
 from models.language_model import LanguageModel
+
 # from models.history_model import HistoryModel
 
 
@@ -11,21 +12,38 @@ translate_controller = Blueprint("translate_controller", __name__)
 # Reqs. 4 e 5
 @translate_controller.route("/", methods=["GET", "POST"])
 def index():
-    languages = LanguageModel.list_dicts()
+    if request.method == "GET":
+        languages = LanguageModel.list_dicts()
+        text_to_translate = "O que deseja traduzir"
+        translated = "Tradução"
 
-    text_to_translate = "O que deseja traduzir"
-    translate_from = "en"
-    translate_to = "pt"
-    translated = "Tradução"
+        return render_template(
+            "index.html",
+            languages=languages,
+            translate_from="pt",
+            translate_to="en",
+            text_to_translate=text_to_translate,
+            translated=translated,
+        )
 
-    return render_template(
-        "index.html",
-        languages=languages,
-        text_to_translate=text_to_translate,
-        translate_from=translate_from,
-        translate_to=translate_to,
-        translated=translated,
-    )
+    elif request.method == "POST":
+        languages = LanguageModel.list_dicts()
+        text_to_translate = request.form.get("text-to-translate")
+        translate_from = request.form.get("translate-from")
+        translate_to = request.form.get("translate-to")
+        translated2 = GoogleTranslator(
+            source=translate_from, target=translate_to
+        )
+        translated3 = translated2.translate(text_to_translate)
+
+        return render_template(
+            "index.html",
+            languages=languages,
+            text_to_translate=text_to_translate,
+            translate_from=translate_from,
+            translate_to=translate_to,
+            translated=translated3,
+        )
 
 
 # Req. 6
